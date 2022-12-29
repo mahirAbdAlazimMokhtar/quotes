6c578ed65caed951f58352537d180998e4ea5663
Mahir Abd Alazim <40815395+mahirAbdAlazimMokhtar@users.noreply.github.com>
2022-12-29 15:48:05 +0300

    add theme class to handle theme for app

---
+++ lib/config/themes/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import '../../core/utils/app_colors.dart';
+
+ThemeData appTheme() {
+  return ThemeData(
+    primaryColor: AppColors.primaryColor,
+    hintColor: AppColors.hintColor,
+  );
+}
