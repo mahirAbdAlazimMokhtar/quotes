5e0e71ffc31b3c35fcee2cc08047460d64d21f62
Mahir Abd Alazim <40815395+mahirAbdAlazimMokhtar@users.noreply.github.com>
2022-12-31 14:00:13 +0300

    edit quote class and add dartz package

--- lib/features/random_quote/domain/entities/quote.dart
+++ lib/features/random_quote/domain/entities/quote.dart
@@ -6,15 +6,18 @@ class Quote extends Equatable {
   final String permalink;
   final int id;
 
-  const Quote(
-      {required this.author,
-      required this.content,
-      required this.permalink,
-      required this.id});
-      
-        @override
-        
-        List<Object?> get props => 
-        throw [author,content,permalink,id];
+  const Quote({
+    required this.author,
+    required this.content,
+    required this.permalink,
+    required this.id,
+  });
 
+  @override
+  List<Object?> get props =>  [
+        author,
+        content,
+        permalink,
+        id,
+      ];
 }
