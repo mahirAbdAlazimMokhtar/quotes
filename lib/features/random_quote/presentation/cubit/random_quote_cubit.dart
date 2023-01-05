835a106e5db8aab6d2d54723e9de0076e8ef246e
Mahir Abd Alazim <40815395+mahirAbdAlazimMokhtar@users.noreply.github.com>
2023-01-05 10:58:24 +0300

    add logic to cubit

--- lib/features/random_quote/presentation/cubit/random_quote_cubit.dart
+++ lib/features/random_quote/presentation/cubit/random_quote_cubit.dart
@@ -2,11 +2,36 @@
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:quotes/core/error/failure.dart';
+import 'package:quotes/core/usecase/usecase.dart';
+import 'package:quotes/core/utils/app_strings.dart';
+import 'package:quotes/features/random_quote/domain/usecases/get_random_quote.dart';
 
 import '../../domain/entities/quote.dart';
 
 part 'random_quote_state.dart';
 
 class RandomQuoteCubit extends Cubit<RandomQuoteState> {
-  RandomQuoteCubit() : super(RandomQuoteInitial());
+  final GetRandomQuote getRandomQuoteUseCase;
+
+  RandomQuoteCubit({required this.getRandomQuoteUseCase})
+      : super(RandomQuoteInitial());
+  Future<void> getRandomQuote() async {
+    var response = await getRandomQuoteUseCase(NoParams());
+    emit(response.fold(
+        (failure) =>
+            RandomQuoteErrorQuote(message: mapFailureToMessage(failure)),
+        (quote) => RandomQuoteLoadedQuote(quoteModel: quote)));
+  }
+
+  String mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return AppStrings.serverFailure;
+      case CacheFailure:
+        return AppStrings.cacheFailure;
+      default:
+        return AppStrings.unexpectedError;
+    }
+  }
 }
